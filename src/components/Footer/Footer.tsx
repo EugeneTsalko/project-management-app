522d661d99ccd10129d3db4c04f93d5371ddfdb8
EugeneTsalko <tefworkmail@gmail.com>
2022-11-17 18:36:06 +0300

    fix: lint-fix

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,12 +4,7 @@ import styles from './Footer.module.scss';
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <a
-        href="https://rs.school/react/"
-        target="_blank"
-        rel="nofollow noreferrer"
-        className={styles.footerLink}
-      >
+      <a href="https://rs.school/react/" target="_blank" rel="nofollow noreferrer" className={styles.footerLink}>
         RSSchool
       </a>
       <div>2022</div>
@@ -22,12 +17,7 @@ export const Footer = () => {
         >
           eugenetsalko
         </a>
-        <a
-          href="https://github.com/hauzinski"
-          target="_blank"
-          rel="nofollow noreferrer"
-          className={styles.footerLink}
-        >
+        <a href="https://github.com/hauzinski" target="_blank" rel="nofollow noreferrer" className={styles.footerLink}>
           hauzinski
         </a>
       </div>
--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,14 +1,7 @@
 import React, { useState } from 'react';
 
 import { useAppSelector, useAppDispatch } from '../../store/hooks';
-import {
-  decrement,
-  increment,
-  incrementByAmount,
-  incrementAsync,
-  incrementIfOdd,
-  selectCount,
-} from './counterSlice';
+import { decrement, increment, incrementByAmount, incrementAsync, incrementIfOdd, selectCount } from './counterSlice';
 import styles from './Counter.module.scss';
 
 export function Counter() {
@@ -21,19 +14,11 @@ export function Counter() {
   return (
     <div>
       <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
+        <button className={styles.button} aria-label="Decrement value" onClick={() => dispatch(decrement())}>
           -
         </button>
         <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
+        <button className={styles.button} aria-label="Increment value" onClick={() => dispatch(increment())}>
           +
         </button>
       </div>
@@ -44,16 +29,10 @@ export function Counter() {
           value={incrementAmount}
           onChange={(e) => setIncrementAmount(e.target.value)}
         />
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
+        <button className={styles.button} onClick={() => dispatch(incrementByAmount(incrementValue))}>
           Add Amount
         </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
+        <button className={styles.asyncButton} onClick={() => dispatch(incrementAsync(incrementValue))}>
           Add Async
         </button>
         <button className={styles.button} onClick={() => dispatch(incrementIfOdd(incrementValue))}>
