5b2c73614e3588ae00085eda69a2822ac81e6533
EugeneTsalko <tefworkmail@gmail.com>
2022-11-18 20:08:20 +0300

    feat: add header style change onscroll

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,10 +3,13 @@ import { NavLink } from 'react-router-dom';
 import styles from './Header.module.scss';
 import logo from '../../assets/logo.svg';
 import { Navigation } from 'components/Navigation/Navigation';
+import { useScrollPosition } from 'utils/hooks/useScrollPosition';
 
 export const Header = () => {
+  const scrollPosition = useScrollPosition();
+
   return (
-    <header className={styles.header}>
+    <header className={scrollPosition > 0 ? `${styles.header} ${styles.scrolled}` : styles.header}>
       <NavLink to="/">
         <img src={logo} alt="logo" />
       </NavLink>
