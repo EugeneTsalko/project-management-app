0194ab42a8e161d805094e98657ef17fef7700f9
EugeneTsalko <tefworkmail@gmail.com>
2022-11-22 14:26:43 +0300

    fix: add yuksak in hero section

--- src/pages/WelcomePage/components/Hero/Hero.tsx
+++ src/pages/WelcomePage/components/Hero/Hero.tsx
@@ -7,33 +7,51 @@ import mailImg from '../../../../assets/mail.svg';
 export const Hero = () => {
   return (
     <section className={styles.hero}>
-      <div className={styles.heroPerson}>
-        <h3>Eugene</h3>
-        <div>Done: Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, ex.</div>
-        <div className={styles.heroPersonContacts}>
-          <img src={githubImg} alt="github" />
-          <a href="https://rs.school/react/" target="_blank" rel="nofollow noreferrer">
-            eugenetsalko
-          </a>
-        </div>
-        <div className={styles.heroPersonContacts}>
-          <img src={mailImg} alt="mail" />
-          <span>tefworkmail@gmail.com</span>
-        </div>
-      </div>
       <img src={heroImg} alt="about image" className={styles.heroImage}></img>
-      <div className={styles.heroPerson}>
-        <h3>Vitali</h3>
-        <div>Done: Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, ex.</div>
-        <div className={styles.heroPersonContacts}>
-          <img src={githubImg} alt="github" />
-          <a href="https://github.com/hauzinski" target="_blank" rel="nofollow noreferrer">
-            hauzinski
-          </a>
+      <div className={styles.heroes}>
+        <div className={styles.heroPerson}>
+          <h3>Eugene</h3>
+          <div>Done: Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, ex.</div>
+          <div className={styles.heroPersonContacts}>
+            <img src={githubImg} alt="github" />
+            <a href="https://rs.school/react/" target="_blank" rel="nofollow noreferrer">
+              eugenetsalko
+            </a>
+          </div>
+          <div className={styles.heroPersonContacts}>
+            <img src={mailImg} alt="mail" />
+            <span>tefworkmail@gmail.com</span>
+          </div>
         </div>
-        <div className={styles.heroPersonContacts}>
-          <img src={mailImg} alt="mail" />
-          <span>hauzinski@gmail.com</span>
+
+        <div className={styles.heroPerson}>
+          <h3>Vitali</h3>
+          <div>Done: Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, ex.</div>
+          <div className={styles.heroPersonContacts}>
+            <img src={githubImg} alt="github" />
+            <a href="https://github.com/hauzinski" target="_blank" rel="nofollow noreferrer">
+              hauzinski
+            </a>
+          </div>
+          <div className={styles.heroPersonContacts}>
+            <img src={mailImg} alt="mail" />
+            <span>hauzinski@gmail.com</span>
+          </div>
+        </div>
+
+        <div className={styles.heroPerson}>
+          <h3>Alisher</h3>
+          <div>Done: Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta, ex.</div>
+          <div className={styles.heroPersonContacts}>
+            <img src={githubImg} alt="github" />
+            <a href="https://github.com/yuksak" target="_blank" rel="nofollow noreferrer">
+              yuksak
+            </a>
+          </div>
+          <div className={styles.heroPersonContacts}>
+            <img src={mailImg} alt="mail" />
+            <span>argulamov@gmail.com</span>
+          </div>
         </div>
       </div>
     </section>
