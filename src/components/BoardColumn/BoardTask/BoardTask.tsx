522253e1479b8ab30144fcade25b0b43eed1bd59
Vitali Hauzinski <hauzinski@gmail.com>
2022-11-29 12:07:45 +0300

    refactoring: add transition to border task component

--- src/components/BoardColumn/BoardTask/BoardTask.tsx
+++ src/components/BoardColumn/BoardTask/BoardTask.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+import { CSSTransition } from 'react-transition-group';
+
 import { RemoveTask } from './ModalWindows/RemoveTask/RemoveTask';
 import { AboutTask } from './ModalWindows/AboutTask/AboutTask';
 import { IoTrash } from 'react-icons/io5';
@@ -11,17 +13,36 @@ const BoardTask = ({ data, boardId, columnId }: BoardTaskProps) => {
   const [removeTaskModalWindow, setRemoveTaskModalWindow] = useState(false);
   const [aboutTaskModalWindow, setAboutTaskModalWindow] = useState(false);
 
+  const deleteButton = React.useRef(null);
+
   return (
     <>
       <div className={styles.main} onMouseEnter={() => setTaskHover(true)} onMouseLeave={() => setTaskHover(false)}>
         <p className={styles.task} onClick={() => setAboutTaskModalWindow(true)}>
           {data.title}
         </p>
-        {isTaskHover && (
-          <button className={styles.deleteTaskButton} type="button" onClick={() => setRemoveTaskModalWindow(true)}>
+
+        <CSSTransition
+          nodeRef={deleteButton}
+          in={isTaskHover}
+          timeout={200}
+          classNames={{
+            enter: styles.deleteTaskButtonEnter,
+            enterActive: styles.deleteTaskButtonEnterActive,
+            exit: styles.deleteTaskButtonExit,
+            exitActive: styles.deleteTaskButtonExitActive,
+          }}
+          unmountOnExit
+        >
+          <button
+            ref={deleteButton}
+            className={styles.deleteTaskButton}
+            type="button"
+            onClick={() => setRemoveTaskModalWindow(true)}
+          >
             <IoTrash />
           </button>
-        )}
+        </CSSTransition>
       </div>
       {removeTaskModalWindow && (
         <RemoveTask setState={setRemoveTaskModalWindow} boardId={boardId} columnId={columnId} taskId={data.id} />
